1fd4dba0e5bde863fdeb0fd195d53d05c1973f95
Ivan Schuetz <ivanschuetz@gmail.com>
2020-03-22 20:17:41 +0100

    Delete storyboard, add home

--- CoEpi/AppDelegate.swift
+++ CoEpi/AppDelegate.swift
@@ -5,26 +5,15 @@ import Dip
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    internal var window: UIWindow?
+
     private let container: DependencyContainer = Dependencies().createContainer()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = initWindow()
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
 
     // MARK: - Core Data stack
 
@@ -71,5 +60,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 
+    // MARK: - Private
+
+    private func initWindow() -> UIWindow {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let homeViewController = HomeViewController.loadFromNib()
+        window.rootViewController = homeViewController
+        window.makeKeyAndVisible()
+        return window
+    }
 }
 
--- CoEpi/ui/home/HomeViewController.swift
+++ CoEpi/ui/home/HomeViewController.swift
@@ -1,11 +1,10 @@
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
 }
