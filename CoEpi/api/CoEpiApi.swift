e4bea774ed9edfc1b18d177e7f3f5c0b70dc319c
Ivan Schütz <ivanschuetz@gmail.com>
2020-04-07 23:11:02 +0200

    Handle HTTP errors and show in alert (#76)

--- CoEpi/api/CoEpiApi.swift
+++ CoEpi/api/CoEpiApi.swift
@@ -33,36 +33,32 @@ class CoEpiApiImpl: CoEpiApi {
     private func get<T: Decodable>(url: String) -> Single<T> {
         Single.create { emitter -> Disposable in
             let request = AF.request(url).responseJSON { response in
-                guard let data = response.data else { return }
-                do {
-                    let decoder = JSONDecoder()
-                    let obj = try decoder.decode(T.self, from: data)
-                    emitter(.success(obj))
-                } catch let error {
-                    emitter(.error(ApiError.error(
-                        message: "Couldn't parse reponse: \(error), " +
-                            "data: \(String(describing: String(data: data, encoding: .utf8)))")))
+                if let apiError = response.apiError {
+                    emitter(.error(apiError))
+                } else {
+                    guard let data = response.data else {
+                        emitter(.error(ApiError.error(message: "Response didn't have data")))
+                        return
+                    }
+                    emitter(processGetResultData(data: data))
                 }
             }
-
             return Disposables.create {
                 request.cancel()
             }
         }
     }
 
-    func post<T: Encodable>(url: String, params: T) -> Completable {
+    private func post<T: Encodable>(url: String, params: T) -> Completable {
         Single<Void>.create { emitter -> Disposable in
             let request = AF.request(url, method: .post, parameters: params, encoder: JSONParameterEncoder.default)
                 .response { response in
-
-                if let error = response.error {
-                    emitter(.error(error))
-                } else {
-                    emitter(.success(()))
+                    if let apiError = response.apiError {
+                        emitter(.error(apiError))
+                    } else {
+                        emitter(.success(()))
+                    }
                 }
-
-            }
             return Disposables.create {
                 request.cancel()
             }
@@ -72,4 +68,36 @@ class CoEpiApiImpl: CoEpiApi {
 
 public enum ApiError: Error {
     case error(message: String)
+}
+
+// TODO better error handling. Ideally map all possible status codes and other errors (timeout etc) to an enum
+private extension AFDataResponse {
+    var apiError: ApiError? {
+        if let error = error {
+            return .error(message: error.localizedDescription)
+        } else {
+            if let statusCode = response?.statusCode {
+                if (200..<400).contains(statusCode) {
+                    return nil
+                } else {
+                    let dataStr: String = data.flatMap { String(data: $0, encoding: String.Encoding.utf8) } ?? "None"
+                    return .error(message: "Http status: \(statusCode), data: \(dataStr)")
+                }
+            } else {
+                os_log("Data response without response. Ignoring. %@", log: servicesLog, type: .debug, "\(self)")
+                return nil
+            }
+        }
+    }
+}
+
+private func processGetResultData<T: Decodable>(data: Data) -> SingleEvent<T> {
+    do {
+        let decoder = JSONDecoder()
+        let obj = try decoder.decode(T.self, from: data)
+        return .success(obj)
+    } catch let error {
+        return .error(ApiError.error(message: "Couldn't parse reponse: \(error), " +
+            "data: \(String(describing: String(data: data, encoding: .utf8)))"))
+    }
 }
