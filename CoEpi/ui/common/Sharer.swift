da8071d82d81192a1bd085fd8c4ced0a8b62392b
Scott Leibrand <scottleibrand@gmail.com>
2020-04-11 23:53:48 -0700

    updated share text and link (#101)

--- CoEpi/ui/common/Sharer.swift
+++ CoEpi/ui/common/Sharer.swift
@@ -5,9 +5,9 @@ class Sharer {
 
     func share(viewController: UIViewController, sourceView: UIView? = nil, completion: (() -> Void)? = nil) {
 
-        if let myWebsite = URL(string: "https://google.com") { // Enter iOS app link here
+        if let myWebsite = URL(string: "https://CoEpi.org") { // Enter iOS app link here
             let activityVC = createActivityViewController(
-                textToShare: "Sharing this app will help slow the spread of Covid-19",
+                textToShare: "Help us test CoEpi: together we can protect each other and our loved ones from the spread of contagious illnesses by anonymously letting others know if we develop symptoms",
                 websiteUrl: myWebsite // Enter iOS app link here
             )
 
