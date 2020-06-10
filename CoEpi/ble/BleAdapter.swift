3c5e45332aa8a6752ee4fd7ab31def53a31ce151
Ivan Schütz <ivanschuetz@gmail.com>
2020-06-10 18:39:50 +0200

    Temporary filter (app v0.3) for repeated TCNs (#257)

--- CoEpi/ble/BleAdapter.swift
+++ CoEpi/ble/BleAdapter.swift
@@ -9,6 +9,9 @@ class BleAdapter {
     let myTcn: ReplaySubject<String> = .create(bufferSize: 1)
 
     init(tcnGenerator: TcnGenerator) {
+
+        var latestTcns = LimitedSizeQueue<Data>(maxSize: 500)
+
         tcnService = TCNBluetoothService(tcnGenerator: { [myTcn] in
             let tcnResult = tcnGenerator.generateTcn()
             log.d("Generated TCN: \(tcnResult)")
@@ -24,7 +27,12 @@ class BleAdapter {
             }()
 
         }, tcnFinder: { [discovered] (data, _) in
-            discovered.onNext(data)
+            // Temporary guard to filter repeated TCNs,
+            // since we don't do anything meaningful with them in v0.3, and this overloads db/logs
+            if !latestTcns.array.contains(data) {
+                discovered.onNext(data)
+                latestTcns.add(value: data)
+            }
         }) { error in
             // TODO What kind of errors? Should we notify the user?
             log.e("TCN service error: \(error)")
