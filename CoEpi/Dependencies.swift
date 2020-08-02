f71049ab2a65155dacfaef895c037ab9f7ba6304
Ivan Schütz <ivanschuetz@gmail.com>
2020-08-02 17:14:20 +0200

    Lint adjustments, more format (#341)
    * Format manually dependencies file
    
    * Only warning for line length
    
    * Move file
    
    * Only error for variable length

--- CoEpi/Dependencies.swift
+++ CoEpi/Dependencies.swift
@@ -18,7 +18,6 @@ class Dependencies {
             distanceShorterThan: Measurement(value: 10, unit: .feet)
         )
 
-        registerLogic(container: container)
         registerWiring(container: container, alertFilterSettings: alertFilterSettings)
         registerViewModels(container: container, alertFilterSettings: alertFilterSettings)
         registerRepos(container: container)
@@ -44,178 +43,136 @@ class Dependencies {
             fatalError("CRITICAL: Couldn't initialize core: \(bootstrapResult)")
         }
 
-        container
-            .register(.singleton) { nativeCore as ServicesBootstrapper }
-        container
-            .register(.singleton) { nativeCore as AlertsApi }
-        container
-            .register(.singleton) { nativeCore as SymptomsInputManager }
-        container
-            .register(.singleton) { nativeCore as ObservedTcnsRecorder }
-        container
-            .register(.singleton) { nativeCore as TcnGenerator }
+        container.register(.singleton) { nativeCore as ServicesBootstrapper }
+        container.register(.singleton) { nativeCore as AlertsApi }
+        container.register(.singleton) { nativeCore as SymptomsInputManager }
+        container.register(.singleton) { nativeCore as ObservedTcnsRecorder }
+        container.register(.singleton) { nativeCore as TcnGenerator }
     }
 
     private func registerViewModels(container: DependencyContainer,
                                     alertFilterSettings: AlertFilterSettings)
     {
-        container.register { HomeViewModel(startPermissions: try container.resolve(),
-                                           rootNav: try container.resolve(),
-                                           alertRepo: try container.resolve(),
-                                           envInfos: try container.resolve()) }
+        container.register { HomeViewModel(
+            startPermissions: try container.resolve(),
+            rootNav: try container.resolve(),
+            alertRepo: try container.resolve(),
+            envInfos: try container.resolve()
+        ) }
         container.register { OnboardingViewModel() }
 
-        container
-            .register { ThankYouViewModel(
-                rootNav: try container.resolve()) }
-        container
-            .register { BreathlessViewModel(
-                symptomFlowManager: try container.resolve()) }
-        container
-            .register { CoughTypeViewModel(
-                symptomFlowManager: try container.resolve()) }
-        container
-            .register { CoughDaysViewModel(
-                symptomFlowManager: try container.resolve()) }
-        container
-            .register { CoughHowViewModel(
-                symptomFlowManager: try container.resolve()) }
-        container
-            .register { FeverDaysViewModel(
-                symptomFlowManager: try container.resolve()) }
-        container
-            .register { FeverTodayViewModel(
-                symptomFlowManager: try container.resolve()) }
-        container
-            .register { FeverWhereViewModel(
-                symptomFlowManager: try container.resolve()) }
-        container
-            .register { FeverTempViewModel(
-                symptomFlowManager: try container.resolve()) }
-        container
-            .register { SymptomReportViewModel(
-                symptomRepo: try container.resolve(),
-                rootNav: try container.resolve(),
-                symptomFlowManager: try container.resolve()
-            ) }
+        container.register { ThankYouViewModel(rootNav: try container.resolve()) }
+        container.register { BreathlessViewModel(symptomFlowManager: try container.resolve()) }
+        container.register { CoughTypeViewModel(symptomFlowManager: try container.resolve()) }
+        container.register { CoughDaysViewModel(symptomFlowManager: try container.resolve()) }
+        container.register { CoughHowViewModel(symptomFlowManager: try container.resolve()) }
+        container.register { FeverDaysViewModel(symptomFlowManager: try container.resolve()) }
+        container.register { FeverTodayViewModel(symptomFlowManager: try container.resolve()) }
+        container.register { FeverWhereViewModel(symptomFlowManager: try container.resolve()) }
+        container.register { FeverTempViewModel(symptomFlowManager: try container.resolve()) }
+        container.register { SymptomReportViewModel(
+            symptomRepo: try container.resolve(),
+            rootNav: try container.resolve(),
+            symptomFlowManager: try container.resolve()
+        ) }
 
-        container
-            .register { OnboardingWireframe(container: container) }
-        container
-            .register { SymptomStartDaysViewModel(
-                symptomFlowManager: try container.resolve()) }
+        container.register { OnboardingWireframe(container: container) }
+        container.register { SymptomStartDaysViewModel(
+            symptomFlowManager: try container.resolve()
+        ) }
 
-        container
-            .register { AlertsViewModel(
-                alertRepo: try container.resolve(),
-                nav: try container.resolve()
-            ) }
+        container.register { AlertsViewModel(
+            alertRepo: try container.resolve(),
+            nav: try container.resolve()
+        ) }
 
-        container
-            .register { DebugBleViewModel(
-                bleAdapter: try container.resolve()) }
-        container
-            .register { LogsViewModel(
-                log: cachingLog, clipboard: try container.resolve(),
-                envInfos: try container.resolve()
-            ) }
+        container.register { DebugBleViewModel(bleAdapter: try container.resolve()) }
+        container.register { LogsViewModel(
+            log: cachingLog,
+            clipboard: try container.resolve(),
+            envInfos: try container.resolve()
+        ) }
 
-        container.register { AlertDetailsViewModel(pars: $0,
-                                                   alertRepo: try container.resolve(),
-                                                   nav: try container.resolve(),
-                                                   email: try container.resolve(),
-                                                   unitsFormatter: try container.resolve()) }
+        container.register { AlertDetailsViewModel(
+            pars: $0,
+            alertRepo: try container.resolve(),
+            nav: try container.resolve(),
+            email: try container.resolve(),
+            unitsFormatter: try container.resolve()
+        ) }
 
-        container.register { UserSettingsViewModel(kvStore: try container.resolve(),
-                                                   alertFilterSettings: alertFilterSettings,
-                                                   envInfos: try container.resolve(),
-                                                   email: try container.resolve(),
-                                                   unitsFormatter: try container.resolve()) }
+        container.register { UserSettingsViewModel(
+            kvStore: try container.resolve(),
+            alertFilterSettings: alertFilterSettings,
+            envInfos: try container.resolve(),
+            email: try container.resolve(),
+            unitsFormatter: try container.resolve()
+        ) }
     }
 
     private func registerRepos(container: DependencyContainer) {
-        container
-            .register(.singleton) { SymptomRepoImpl(
-                inputManager: try container.resolve()) as SymptomRepo }
-        container
-            .register(.singleton) { AlertRepoImpl(
-                alertsApi: try container.resolve(),
-                notificationShower: try container.resolve(),
-                alertFilters: try container.resolve()
-            ) as AlertRepo
-            }
+        container.register(.singleton) { SymptomRepoImpl(
+            inputManager: try container.resolve()
+        ) as SymptomRepo }
+        container.register(.singleton) { AlertRepoImpl(
+            alertsApi: try container.resolve(),
+            notificationShower: try container.resolve(),
+            alertFilters: try container.resolve()
+        ) as AlertRepo }
     }
 
     private func registerServices(container: DependencyContainer) {
-        container
-            .register(.singleton) { AppBadgeUpdaterImpl() as AppBadgeUpdater }
-        container
-            .register(.singleton) { NotificationShowerImpl() as NotificationShower }
+        container.register(.singleton) { AppBadgeUpdaterImpl() as AppBadgeUpdater }
+        container.register(.singleton) { NotificationShowerImpl() as NotificationShower }
 
-        container
-            .register(.singleton) { BackgroundTasksManager() }
-        container
-            .register(.eagerSingleton) { FetchAlertsBackgroundRegisterer(
-                tasksManager: try container.resolve(),
-                alertRepo: try container.resolve()
-            ) }
+        container.register(.singleton) { BackgroundTasksManager() }
+        container.register(.eagerSingleton) { FetchAlertsBackgroundRegisterer(
+            tasksManager: try container.resolve(),
+            alertRepo: try container.resolve()
+        ) }
         container.register(.eagerSingleton) { NotificationsDelegate(
-            rootNav: try container.resolve()) }
-    }
-
-    private func registerLogic(container _: DependencyContainer) {
-//        container.register(.eagerSingleton) { AlertNotificationsShower(alertsRepo: try container.resolve()) }
+            rootNav: try container.resolve()
+        ) }
     }
 
     private func registerWiring(
         container: DependencyContainer,
         alertFilterSettings: AlertFilterSettings
     ) {
-        container
-            .register(.eagerSingleton) { ScannedCensHandler(
-                bleAdapter: try container.resolve(),
-                tcnsRecorder: try container.resolve()
-            ) }
-        container
-            .register(.eagerSingleton) { PeriodicAlertsFetcher(
-                alertRepo: try container.resolve()) }
-        container
-            .register(.eagerSingleton) { RootNav() }
-
-        container
-            .register(.singleton) { SymptomRouterImpl(
-                rootNav: try container.resolve()) as SymptomRouter }
-        container
-            .register(.singleton) { SymptomFlowManager(
-                symptomRouter: try container.resolve(),
-                rootNavigation: try container.resolve(),
-                inputsManager: try container.resolve()
-            ) }
-        container.register {
-            ObservableAlertFiltersImpl(
-                kvStore: try container.resolve(),
-                filterSettings: alertFilterSettings
-            ) as ObservableAlertFilters
+        container.register(.eagerSingleton) { ScannedCensHandler(
+            bleAdapter: try container.resolve(), tcnsRecorder: try container.resolve()
+        )
         }
+        container.register(.eagerSingleton) { PeriodicAlertsFetcher(
+            alertRepo: try container.resolve()
+        ) }
+        container.register(.eagerSingleton) { RootNav() }
+
+        container.register(.singleton) { SymptomRouterImpl(
+            rootNav: try container.resolve()
+        ) as SymptomRouter }
+        container.register(.singleton) { SymptomFlowManager(
+            symptomRouter: try container.resolve(),
+            rootNavigation: try container.resolve(),
+            inputsManager: try container.resolve()
+        )
+        }
+        container.register { ObservableAlertFiltersImpl(
+            kvStore: try container.resolve(),
+            filterSettings: alertFilterSettings
+        ) as ObservableAlertFilters }
     }
 
     private func registerBle(container: DependencyContainer) {
-        container
-            .register(.eagerSingleton) { BleAdapter(
-                tcnGenerator: try container.resolve()) }
+        container.register(.eagerSingleton) { BleAdapter(tcnGenerator: try container.resolve()) }
     }
 
     private func registerSystem(container: DependencyContainer) {
-        container
-            .register(.singleton) { KeyValueStoreImpl() as KeyValueStore }
-        container
-            .register(.singleton) { StartPermissionsImpl() as StartPermissions }
-        container
-            .register(.singleton) { Clipboard() }
-        container
-            .register(.singleton) { EnvInfos() }
-        container
-            .register(.singleton) { EmailImpl() as Email }
+        container.register(.singleton) { KeyValueStoreImpl() as KeyValueStore }
+        container.register(.singleton) { StartPermissionsImpl() as StartPermissions }
+        container.register(.singleton) { Clipboard() }
+        container.register(.singleton) { EnvInfos() }
+        container.register(.singleton) { EmailImpl() as Email }
         container.register(.singleton) { ObservableKeyValueStoreImpl(
             keyValueStore: try container.resolve()) as ObservableKeyValueStore
         }
