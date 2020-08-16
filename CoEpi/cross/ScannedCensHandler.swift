5585271e735f664cb3c92c2adecd0b77be6d3aac
Dusko Ojdanic <dusko.ojdanic@gmail.com>
2020-08-16 15:07:39 +0200

    forwardScannedCensToCoEpiRepo with groupBy and throttle (#360)

--- CoEpi/cross/ScannedCensHandler.swift
+++ CoEpi/cross/ScannedCensHandler.swift
@@ -15,16 +15,38 @@ class ScannedCensHandler {
     }
 
     private func forwardScannedCensToCoEpiRepo() {
+        let backgroundScheduler = ConcurrentDispatchQueueScheduler(qos: .background)
         bleAdapter.discovered
-            .subscribe(onNext: { [tcnsRecorder] data, distance in
-                let res = tcnsRecorder.recordTcn(tcn: data, distance: distance)
-
-                if !res.isSuccess() {
-                    log.e("Error recording TCN: \(res)")
+            //use 'data' (tcn value) as grouping key (http://reactivex.io/documentation/operators/groupby.html, https://github.com/ReactiveX/RxSwift/blob/master/RxSwift/Observables/GroupBy.swift)
+            .groupBy(keySelector: { data, distance in data })
+            .subscribe { event in
+                switch event {
+                case .next(let group):
+                    group
+                        //take one sample per tcn value every 5 sec
+                        .throttle(.milliseconds(5000), latest: false, scheduler: backgroundScheduler)
+                        .subscribe { innerEvent in
+                            switch innerEvent {
+                            case .next(let dataDistanceTuple):
+//                                    print("data: \(dataDistanceTuple.0.toHex()), distance: \(dataDistanceTuple.1)")
+                                let tcnRecordingResult = self.tcnsRecorder.recordTcn(tcn: dataDistanceTuple.0, distance: dataDistanceTuple.1)
+                                if !tcnRecordingResult.isSuccess() {
+                                    log.e("Error recording TCN: \(tcnRecordingResult)")
+                                }
+                            case .error(let error):
+                                log.e("Error in grouped tcn observer: \(error.localizedDescription)")
+                            case .completed:
+                                log.d("Inner observable completed")
+                            }
+                        }
+                        .disposed(by: self.disposeBag)
+                case .error(let error):
+                    log.e("Error in central cen observer: \(error.localizedDescription)")
+                case .completed:
+                    log.d("Outer observable completed")
                 }
+            }
+            .disposed(by: disposeBag)
 
-            }, onError: { error in
-                log.e("Error in central cen observer: \(error.localizedDescription)")
-            }).disposed(by: disposeBag)
     }
 }
